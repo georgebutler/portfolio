7c928bea95758d45b94e0df7411a00d90687c837
George Butler <georgebutleriv@gmail.com>
2022-06-10 19:21:38 -0700

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="page-wrapper with-navbar">
+    <nav class="navbar">
+      <div class="navbar-content">
+        <button class="btn btn-action" type="button">
+          <i class="fa fa-bars" aria-hidden="true" />
+          <span class="sr-only">Toggle sidebar</span>
+        </button>
+      </div>
+    </nav>
+
+    <div class="content-wrapper">
+      <Tutorial />
+      <div class="alert" role="alert">
+        <h4 class="alert-heading">
+          Default alert
+        </h4>
+        This is a default alert with some content and <a href="#" class="alert-link">a link</a>.
+      </div>
+      <div class="alert alert-primary" role="alert">
+        <h4 class="alert-heading">
+          Primary alert
+        </h4>
+        This is a primary alert with some content and <a href="#" class="alert-link">a link</a>.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
